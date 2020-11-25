493ab0882f2af02f1969c0392c363336ac3e34ea
Dan Chiev <danchiev2@Gmail.com>
2020-11-25 18:31:11 -0500

    added views

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,78 @@
-var connection = require("./connection.js");
+var connection = require("../config/connection.js");
 
-var orm = {
-    selectWhere: function(tableInput,colToSearch,valOfCol){
-        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-        connection.query(queryString,[tableInput,colToSearch,valOfCol], function (err,result){
-            if (err) throw err;
-            console.log(result);
-        });
+function printQuestionMarks(num) {
+  var arr = [];
 
-    },
-    insertOne: function(whatToSelect,table,orderCol) {
-        var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
-        console.log(queryString);
-        connection.query(queryString,[whatToSelect,table,orderCol], function (err,result){
-            if (err) throw err;
-            console.log(result);
-        });
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-        
-    },
-    updateOne: function(tableInput,colToSearch,valOfCol){
-        var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-        connection.query(queryString,[tableInput,colToSearch,valOfCol], function (err,result){
-            if (err) throw err;
-            console.log(result);
-        });
-
-    }
+  return arr.toString();
 }
 
+function objToSql(ob) {
+  var arr = [];
 
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
 
+  return arr.toString();
+}
 
+var orm = {
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
 
 module.exports = orm;
