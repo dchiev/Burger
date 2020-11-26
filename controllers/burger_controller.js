690a872a371bb47950b3144bdcfb183038df06bb
Dan Chiev <danchiev2@Gmail.com>
2020-11-25 19:31:48 -0500

    front end functional

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -4,38 +4,41 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
-router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
+router.get("/", function (req, res) {
+  burger.selectAll(function (data) {
     var hbsObject = {
-        burgers: data
+      burgers: data,
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-    burger.insertOne(["name", "devoured"], [req.body.name, req.body.devoured], function(result) {
-    res.json({ id: result.insertId });
-  });
+router.post("/api/burgers", function (req, res) {
+  burger.insertOne(
+    ["name", "devoured"],
+    [req.body.name, req.body.devoured],
+    function (result) {
+      res.json({ id: result.insertId });
+    }
+  );
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.updateOne(
     {
-      devoured: req.body.devoured
+      devoured: req.body.devoured,
     },
     condition,
-    function(result) {
+    function (result) {
       if (result.changedRows === 0) {
         return res.status(404).end();
       }
       res.status(200).end();
-
     }
   );
 });
